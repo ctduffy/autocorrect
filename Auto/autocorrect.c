f82b3061d24e2ae2da6cdd71050e32953f6d8050
Clara <cduffy@students.stab.org>
2016-10-10 11:43:15 -0400

    next

--- Auto/autocorrect.c
+++ Auto/autocorrect.c
@@ -35,7 +35,7 @@ linked_t autocomplete(linked_t list, trie_t trie, char* curr, data_t data){
 	}
 	else{
 		if(trie!=NULL){
-			return autocomplete(list, trie, curr[1], data->word);
+			return autocomplete(list, trie, curr[1], data);
 		}
 	}
 }
