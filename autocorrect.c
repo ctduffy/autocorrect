b1dbd624576ee284eec88e2f181bf081ac61eb36
Clara <cduffy@students.stab.org>
2016-10-10 18:57:44 -0400

    hello

--- autocorrect.c
+++ autocorrect.c
@@ -38,6 +38,7 @@ linked_t autocomplete(linked_t list, trie_t trie, char* curr, data_t data){
 	if(curr == NULL){
 
 	}
+	printf("curr is %c\n", curr[0]);
 	else if(&curr[0] == "o"){
 		if(get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)) != 0 && get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)) != NULL){
 			return linked_add(list, get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)), data->word);
@@ -46,7 +47,6 @@ linked_t autocomplete(linked_t list, trie_t trie, char* curr, data_t data){
 	}
 	else{
 		if(trie!=NULL){
-			printf("nxt is %s\n", &curr[1]);
 			return autocomplete(list, trie, &curr[1], data);
 		}
 	}
