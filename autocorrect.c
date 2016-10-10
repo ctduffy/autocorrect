cf50ddb9371a835eb3fa6862106155a42738f4ca
Clara <cduffy@students.stab.org>
2016-10-10 18:18:30 -0400

    hello

--- autocorrect.c
+++ autocorrect.c
@@ -11,47 +11,7 @@ struct curr_data{
 	trie_t start;
 };
 
-linked_t trie_search(linked_t list, trie_t trie, char* word){
-	if(trie!=NULL){
-		char* wordnext = word;
-		while(wordnext[0] != NULL){
-			trie = get_next_trie(trie, (int) word[0] - 97);
-		}
-		int i;
-		int len = strlen(word);
-		char* newword = word;
-		for(i = 0; i < 26; i++){
-			newword[len] = (char) i + 97;
-			newword[len + 1] = '\0';
-			return trie_search(list, trie, newword);
-		}
-		if(get_trie_frequency(trie) > 0){
-			return linked_add(list, get_trie_frequency(trie), word);
-		}
-	}
-}
-
-linked_t autocomplete(linked_t list, trie_t trie, char* curr, data_t data){
-	printf("DID IT MAKE IT HERE");
-	if((int)strlen(curr) == 1){
-		if(get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)) != 0 && get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)) != NULL){
-			return linked_add(list, get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)), data->word);
-		}
-		return trie_search(list, trie, data->word);
-	}
-	else{
-		if(trie!=NULL){
-			return autocomplete(list, trie, curr[1], data);
-		}
-	}
-}
-
-linked_t autocorrect(linked_t list, trie_t trie, char* curr, int maxld){
-
-}
-
 int main(){
-
 	printf("1here?");
 
 	linked_t linked = linked_create();
@@ -74,3 +34,46 @@ int main(){
 	return 0;
 
 }
+
+linked_t trie_search(linked_t list, trie_t trie, char* word){
+	if(trie!=NULL){
+		char* wordnext = word;
+		while(wordnext[0] != NULL){
+			trie = get_next_trie(trie, (int) word[0] - 97);
+		}
+		int i;
+		int len = strlen(word);
+		char* newword = word;
+		for(i = 0; i < 26; i++){
+			newword[len] = (char) i + 97;
+			newword[len + 1] = '\0';
+			return trie_search(list, trie, newword);
+		}
+		if(get_trie_frequency(trie) > 0){
+			return linked_add(list, get_trie_frequency(trie), word);
+		}
+	}
+}
+
+linked_t autocomplete(linked_t list, trie_t trie, char* curr, data_t data){
+	printf("DID IT MAKE IT HERE");
+	if(curr == NULL){
+
+	}
+	else if(curr == "o"){
+		if(get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)) != 0 && get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)) != NULL){
+			return linked_add(list, get_trie_frequency(get_next_trie(trie, (int) curr[0] - 97)), data->word);
+		}
+		return trie_search(list, trie, data->word);
+	}
+	else{
+		if(trie!=NULL){
+			return autocomplete(list, trie, curr[1], data);
+		}
+	}
+}
+
+linked_t autocorrect(linked_t list, trie_t trie, char* curr, int maxld){
+
+}
+
