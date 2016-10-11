30d9509386e55c723ee4f9a22e8c4ff3d91099c9
Clara <cduffy@students.stab.org>
2016-10-10 22:15:34 -0400

    hello

--- autocorrect.c
+++ autocorrect.c
@@ -33,7 +33,8 @@ linked_t trie_search(linked_t list, trie_t trie, char* word){
 	//int len = (int) strlen(word);
 	//char* newword = word;
 	for(i = 0; i<26; i++){
-		char th[2] = {((char) i + 97), 'A'};
+		char th[2];
+		strcpy(th, (char) i + 97));
 		char* newword = strncat(word, th, 1);
 		return trie_search(list, trie, newword);
 	}
