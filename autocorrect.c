655127870e4699387f61a1556cf63e6fab27c23b
Clara <cduffy@students.stab.org>
2016-10-14 11:45:35 -0400

    hello

--- autocorrect.c
+++ autocorrect.c
@@ -64,7 +64,7 @@ linked_t autocomplete(linked_t list, trie_t trie, char* curr, data_t data){
 				return autocomplete(list, get_next_trie(trie, (int) curr[0] - 97), &curr[0], data);
 			}
 			else{
-				return autocomplete(list, get_next_trie(trie, (int) curr[0] - 97), &curr[1], data);
+				return autocomplete(list, get_next_trie(trie, (int) curr[1] - 97), &curr[1], data);
 			}
 		}
 	}
