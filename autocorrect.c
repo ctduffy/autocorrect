ae3096b2e7f60180a8a18bb56d2cafd181e10fd9
Clara <cduffy@students.stab.org>
2016-11-05 15:46:06 -0400

    neext

--- autocorrect.c
+++ autocorrect.c
@@ -54,7 +54,7 @@ int trie_search(linked_t list, trie_t trie, char* word){
 			now = get_next_trie(now, (int) nowword[0] - 97); //move down the trie one letter, to the first letter of nowword
 		}
 		else if(nowword[1] == NULL){
-
+			nowword = &nowword[1];
 		}
 		else if(get_letter(get_next_trie(now, (int) nowword[1] - 97)) != NULL){ //else if the trie node at the next letter in nowword isnt null
 			now = get_next_trie(now, (int) nowword[1] - 97); //move down the trie to that
