677b1a6471d92d85368d2dbc8f8b137a0bcf0a20
Clara <cduffy@students.stab.org>
2016-11-05 15:04:05 -0400

    neext

--- autocorrect.c
+++ autocorrect.c
@@ -32,7 +32,6 @@ void wordmaker(char* word, trie_t now, trie_t trie, linked_t list){
 	    nex[len] = n;
 	    nex[len+1] = '\0';
 	    if(get_letter(get_next_trie(now, i)) != NULL){
-	    	printf("nex is%s\n", nex);
 			trie_search(list, trie, nex);
 	    }
 	}
